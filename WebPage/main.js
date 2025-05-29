6accdbd351ca5a64e67324a5726e074884232ec2
BoaN235 <noahgoata@outlook.com>
2025-05-28 20:24:03 -0400

    I added a nav bar and made it slide in

---
+++ WebPage/main.js
@@ -0,0 +1,54 @@
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+const h1Element = document.getElementById("hacker_title");
+console.log(h1Element)
+const hiddenText = h1Element.textContent;
+
+let previousTimestamp = 1000;
+let elapsedSeconds = 0;
+let secondspercharacterReveal = 0.1;
+
+
+const animationLoop = (currentTimestamp) => {
+    let secondsSinceLastFrame = (currentTimestamp - previousTimestamp) / 1000;
+    previousTimestamp = currentTimestamp
+    
+    elapsedSeconds += secondsSinceLastFrame
+
+    const charctersToReveal = elapsedSeconds/secondspercharacterReveal
+
+    let animatedText = ""
+
+    for (let i = 0; i < hiddenText.length; i++ ) {
+        let character = ""
+
+        if ( i < charctersToReveal) {
+            character = hiddenText[i]
+        }
+        else {
+            character = alphabet[Math.floor(Math.random() * alphabet.length)]
+        }
+        animatedText += character;
+    }
+    h1Element.innerText = animatedText
+    
+    window.requestAnimationFrame(animationLoop);
+}
+window.requestAnimationFrame(animationLoop)
+
+h1Element.addEventListener("click", () => {
+    elapsedSeconds = 0;
+})
+
+
+function home() {
+window.location.href = "index.html";
+}
+function projects() {
+window.location.href = "project_list.html";
+}
+function blog() {
+window.location.href = "index.html";
+}
+function about() {
+window.location.href = "index.html";
+}
